c43ec3345f416eb06fae669e03d7de8a5a56a017
Brian Garcia <bjgarc@gmail.com>
2020-02-11 16:29:42 -0700

    redirect uri change

--- pkg/encryption/cipher_test.go
+++ pkg/encryption/cipher_test.go
@@ -46,7 +46,16 @@ func TestEncodeAndDecodeAccessTokenB64(t *testing.T) {
 func TestDecrypt(t *testing.T) {
 	const secret = "2dccd1ab3e03990aea77359831c85ca2"
 	const token = "/CG8pcyYZdf7yLnTaCmM9sxZOo0zpDQ7a5W5Gq8AfcQ6aOJg5F3NRLgf+fSFXqg6Fug6IcmiPqO7jKyU61oWFdcC0l3rJ2zSFuBPhcwtLJ+dTTjayOVrqLkjfjEILBska+j+5iz3IuYACmpOFFhIHhfJwmDTsP5emtmmNnch9vFkwdLZSHFauWlAgDsWpziOZ+KslGBK4imYqAIiC2geXjQ8tQ0XeXgGRl3aZrpPDQXmgIRgjUWV0e1G//2ODC25Az03GSyks0BB94S+sAc0uUyUen5kD8THpxAL9/dx74KOo/qBXqrQxQLoBg5kmIbKuXhJLt/fseB0SUuczxN8T/o8jGR1RSu9wowPYmYsp0v341403u8RlDFZsP4tooSMOxWg6Wrz/ShnQZ7eO+PvG8+Bn9rv2a5cfc7cny03Gm4DJPG3sUv5jwmvTfk8VpF62upwi1K8bwnbAgfQ2/RMCyuVV9lufdCcu2x6CRwOku52krCEzgbSahEBJ3o769SK/Lep2sOtQTBmJi2NP40LWX61eVun1rmZVe8buS96tlhj4vco9B/rPtIB/uzI8Y6XCyJJzkt/jZ8uRzPUmbGpzrNg8MwIDFk2VarTW2BCoRJkJCICFjb76lQwJ3XB2URxu8wJr8t+u/jENegAEnSe+XLEzsbGO9ZXE3NTpGG8GoyrnB/bc6Y/fWtMAzMvz2026EBR3KLd4pQLUBobQFouLo04qYuKDaxBruSu5wNT6VzkTSz4iIXpekDyDpEJQ9xbSP7ETzXaJRyll6KD1v1HpHuZWliVgpM+ROaspoSpQrig6HNMUPfjROJBxoaamKJHEXvjTjsWfdaLugZRO50kXbKT8ckkipA+vtzSclE28zfk54qYcjwCffQfwbU+3tt63M+6MFeDPTxCxg1/8532SqBYg1jAOaeNPyYMKX7gNHv31iBezIkHFJxkPGo4l2cTkoJgO32fmzPUryOdY9BqCxjuGdPXlGP/2G7d16wUcrZfv5d3n6m1ZkoJuUa55m3/F2+tHo5JgHk0EVQqz2iq+HFdatNDjsZ947KmAGF0JmyZnlpMT7jWSYVpzS3/V9VFnH76vUn29Nqpbwp0me3O+QydUIq6xQ=="
-	c, err := NewCipher([]byte(secret))
+
+	var s64 []byte
+	b, err := base64.URLEncoding.DecodeString(addPadding(secret))
+	if err == nil {
+		s64 = b
+	} else {
+		s64 = []byte(secret)
+	}
+
+	c, err := NewCipher([]byte(s64))
 	if err != nil {
 		t.Error(err)
 		t.Fail()
@@ -57,4 +66,17 @@ func TestDecrypt(t *testing.T) {
 		t.Fail()
 	}
 	fmt.Println(val)
+}
+func addPadding(secret string) string {
+	padding := len(secret) % 4
+	switch padding {
+	case 1:
+		return secret + "==="
+	case 2:
+		return secret + "=="
+	case 3:
+		return secret + "="
+	default:
+		return secret
+	}
 }
